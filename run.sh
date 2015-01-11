eca58a6d4f8848d4db92bc25b53ae59ba53ed7fa
David Ogutu <davidogutu@gmail.com>
2015-01-11 02:37:21 -0500

    corrected typo

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ seq -w 1 99999999 > schools.txt
 
 # split into smaller files for parallelization
 echo "splitting up index numbers"
-splt -d -l 50 schools.txt split-
+spilt -d -l 50 schools.txt split-
 
 # run in parallel
 # max of 200 concurrent jobs
