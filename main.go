702a77313f8bb6ee9ce3e21139698af2a392e990
Sam Gichohi <sam@dstillery.com>
2015-01-25 09:31:14 -0500

    kinda works

--- main.go
+++ main.go
@@ -7,15 +7,17 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/puerkitobio/goquery"
 )
 
 const (
-	POSTDATA    = "&ctl00%24cphMain%24TabContainer1%24Marks%24ddlYear=2014&ctl00%24cphMain%24TabContainer1%24Marks%24btnFind=Find"
-	KNEC_URL    = "http://www.knec-portal.ac.ke/RESULTS/ResultKCPE.aspx"
-	MAXROUTINES = 1000
+	POSTDATA     = "&ctl00%24cphMain%24TabContainer1%24Marks%24ddlYear=2014&ctl00%24cphMain%24TabContainer1%24Marks%24btnFind=Find"
+	KNEC_URL     = "http://www.knec-portal.ac.ke/RESULTS/ResultKCPE.aspx"
+	MAXROUTINES  = 1000
+	STUDENTERROR = 2
 )
 
 /*given an index number return a candidates results in a html page */
@@ -47,62 +49,147 @@ func getCandidateResults(index string, client *http.Client) (htmlPage string, er
 	return string(restr), nil
 }
 
-func getIndexNumbers() chan string {
+func getCountyNumbers() chan string {
 
 	ch := make(chan string) // for generator
 
-	COUNTY_NUMBERS := []string{
-		"01101", "01113", "01114", "01115", "02105", "02109", "02110", "03106", "03108",
-		"03120", "03121", "04102", "04107", "04111", "04116", "04119", "04122", "05103",
-		"05112", "05117", "06104", "06118", "07201", "07209", "07213", "07214", "07215",
-		"07216", "07225", "08202", "08210", "08217", "08218", "08219", "08220", "08221",
-		"08237", "09203", "09222", "09223", "09224", "09239", "10204", "10208", "10225",
-		"10227", "10228", "10229", "10234", "10238", "11205", "11207", "11211", "11212",
-		"11230", "11231", "11232", "11233", "11235", "11235", "12301", "12314", "12315",
-		"12315", "12329", "12330", "12343", "12345", "13302", "13310", "13317", "13328",
-		"13331", "13332", "13338", "13339", "13344", "13350", "13351", "13352", "13353",
-		"13354", "13357", "13350", "14303", "14312", "14333", "14341", "14355", "15304",
-		"15309", "15318", "15319", "15327", "15334", "15337", "15349", "15305", "15311",
-		"15320", "15321", "15340", "16358", "15359", "17305", "17322", "17355", "18307",
-		"18323", "18324", "18325", "18335", "18336", "18346", "18347", "18348", "19308",
-		"19313", "19325", "19342", "20401", "20402", "20403", "20404", "20405", "20405",
-		"20407", "20408", "20409", "21501", "21524", "21525", "21548", "21549", "21550",
-		"22502", "22525", "22527", "23503", "23528", "23529", "24505", "24530", "24531",
-		"24568", "25508", "25533", "25551", "25553", "25509", "25534", "25535", "27511",
-		"27535", "27537", "27538", "27552", "27554", "27555", "27559", "27570", "28512",
-		"28522", "28539", "28553", "28571", "29513", "29523", "29540", "29541", "29542",
-		"30514", "30543", "30544", "30555", "30555", "31515", "31545", "31557", "31566",
-		"31567", "32516", "32519", "32546", "32560", "33517", "33521", "33532", "33547",
-		"33558", "33562", "34518", "34520", "34559", "34561", "35601", "35606", "35609",
-		"35610", "35620", "35623", "35629", "36602", "36605", "36611", "36612", "36613",
-		"36621", "36626", "36628", "36630", "37603", "37607", "37608", "37614", "37615",
-		"37616", "37617", "37624", "37625", "37627", "37631", "37532", "38504", "38618",
-		"38519", "38622", "39701", "39713", "39714", "39733", "39734", "39737", "40703",
-		"40711", "40715", "40719", "40723", "40727", "40732", "40735", "40740", "41704",
-		"41709", "41710", "41724", "41730", "41731", "42705", "42712", "42721", "42725",
-		"42725", "42738", "43705", "43715", "43720", "43722", "43728", "44707", "44708",
-		"44717", "44718", "44729", "44736", "44739", "45301", "45304", "45305", "45806",
-		"45815", "45816", "45821", "46802", "46807", "46308", "46809", "46813", "46818",
-		"46819", "46320", "47803", "47810", "47311", "47812", "47814", "47817"}
+	COUNTY_NUMBERS := []string{"01113"}
+	/*COUNTY_NUMBERS := []string{"01101"} , "01113", "01114", "01115", "02105", "02109", "02110", "03106", "03108",
+	"03120", "03121", "04102", "04107", "04111", "04116", "04119", "04122", "05103",
+	"05112", "05117", "06104", "06118", "07201", "07209", "07213", "07214", "07215",
+	"07216", "07225", "08202", "08210", "08217", "08218", "08219", "08220", "08221",
+	"08237", "09203", "09222", "09223", "09224", "09239", "10204", "10208", "10225",
+	"10227", "10228", "10229", "10234", "10238", "11205", "11207", "11211", "11212",
+	"11230", "11231", "11232", "11233", "11235", "11235", "12301", "12314", "12315",
+	"12315", "12329", "12330", "12343", "12345", "13302", "13310", "13317", "13328",
+	"13331", "13332", "13338", "13339", "13344", "13350", "13351", "13352", "13353",
+	"13354", "13357", "13350", "14303", "14312", "14333", "14341", "14355", "15304",
+	"15309", "15318", "15319", "15327", "15334", "15337", "15349", "15305", "15311",
+	"15320", "15321", "15340", "16358", "15359", "17305", "17322", "17355", "18307",
+	"18323", "18324", "18325", "18335", "18336", "18346", "18347", "18348", "19308",
+	"19313", "19325", "19342", "20401", "20402", "20403", "20404", "20405", "20405",
+	"20407", "20408", "20409", "21501", "21524", "21525", "21548", "21549", "21550",
+	"22502", "22525", "22527", "23503", "23528", "23529", "24505", "24530", "24531",
+	"24568", "25508", "25533", "25551", "25553", "25509", "25534", "25535", "27511",
+	"27535", "27537", "27538", "27552", "27554", "27555", "27559", "27570", "28512",
+	"28522", "28539", "28553", "28571", "29513", "29523", "29540", "29541", "29542",
+	"30514", "30543", "30544", "30555", "30555", "31515", "31545", "31557", "31566",
+	"31567", "32516", "32519", "32546", "32560", "33517", "33521", "33532", "33547",
+	"33558", "33562", "34518", "34520", "34559", "34561", "35601", "35606", "35609",
+	"35610", "35620", "35623", "35629", "36602", "36605", "36611", "36612", "36613",
+	"36621", "36626", "36628", "36630", "37603", "37607", "37608", "37614", "37615",
+	"37616", "37617", "37624", "37625", "37627", "37631", "37532", "38504", "38618",
+	"38519", "38622", "39701", "39713", "39714", "39733", "39734", "39737", "40703",
+	"40711", "40715", "40719", "40723", "40727", "40732", "40735", "40740", "41704",
+	"41709", "41710", "41724", "41730", "41731", "42705", "42712", "42721", "42725",
+	"42725", "42738", "43705", "43715", "43720", "43722", "43728", "44707", "44708",
+	"44717", "44718", "44729", "44736", "44739", "45301", "45304", "45305", "45806",
+	"45815", "45816", "45821", "46802", "46807", "46308", "46809", "46813", "46818",
+	"46819", "46320", "47803", "47810", "47311", "47812", "47814", "47817"}*/
 
 	go func() {
-		for _, countyIndex := range COUNTY_NUMBERS {
-			// loop through all possible schools
-			for i := 1; i < 1000; i++ {
-				//format this in a way where we can fast forward to a new school
-				var schoolIndex string = "000" + strconv.Itoa(i)
-				schoolIndex = schoolIndex[(len(schoolIndex) - 3):]
-				// for each school, loop through every possible student
-				for s := 1; s < 1000; s++ {
-					var candidateIndex string = "000" + strconv.Itoa(s)
-					candidateIndex = candidateIndex[(len(candidateIndex) - 3):]
-					ch <- (countyIndex + schoolIndex + candidateIndex)
-				}
-			}
+		for _, v := range COUNTY_NUMBERS {
+			ch <- v
 		}
+		close(ch)
 	}()
 
 	return ch
+
+}
+
+func getIndexNumbers(countyIndex string, errorChan chan bool) chan string {
+
+	ch := make(chan string)
+
+	go func() {
+
+		// loop through all possible schools
+
+		for i := 117; i < 1000; i++ {
+
+			var errCount uint64 = 0
+			fmt.Println("New School")
+			var schoolIndex string = "000" + strconv.Itoa(i)
+			schoolIndex = schoolIndex[(len(schoolIndex) - 3):]
+			move := make(chan bool, 1)
+			studentInd := genCandidateIndex(move)
+			move <- false
+			// for each school, loop through every possible student
+			for candidateIndex := range studentInd {
+				fmt.Println("waiting")
+				if e := <-errorChan; e == true {
+					fmt.Println("error")
+					errCount += 1
+					if errCount > STUDENTERROR {
+						move <- true
+					}
+
+				}
+				fmt.Println("candidateIndex: " + candidateIndex)
+				ch <- (countyIndex + schoolIndex + candidateIndex)
+				move <- false
+			}
+		}
+
+		close(ch)
+
+	}()
+
+	return ch
+}
+
+func genCandidateIndex(move chan bool) chan string {
+
+	res := make(chan string)
+	candidates := make(map[int][]int)
+
+	candidates[0] = append(candidates[0], 1)
+	candidates[0] = append(candidates[0], 2)
+	candidates[0] = append(candidates[0], 3)
+	candidates[1] = append(candidates[1], 11)
+	candidates[1] = append(candidates[1], 12)
+	candidates[1] = append(candidates[1], 13)
+	candidates[2] = append(candidates[2], 100)
+	candidates[2] = append(candidates[2], 101)
+	candidates[2] = append(candidates[2], 102)
+	/*for i := 0; i < 300; i++ {
+		candidates[0] = append(candidates[0], i+1)
+	}
+	for i := 300; i < 600; i++ {
+		candidates[1] = append(candidates[1], i+1)
+	}
+	for i := 700; i < 800; i++ {
+		candidates[2] = append(candidates[1], i+1)
+	}
+	for i := 800; i < 900; i++ {
+		candidates[3] = append(candidates[1], i+1)
+	}*/
+	go func() {
+	Cands:
+		for _, val := range candidates {
+
+			for _, index := range val {
+
+				yes := <-move
+				fmt.Println("moving", yes)
+				if yes {
+					continue Cands
+				}
+				var candidateIndex string = "000" + strconv.Itoa(index)
+				candidateIndex = candidateIndex[(len(candidateIndex) - 3):]
+				res <- candidateIndex
+			}
+
+		}
+		/*
+			res <- "001"
+			res <- "002"*/
+		close(res)
+	}()
+
+	return res
+
 }
 
 /* Parse html page and give  results in a CSV format */
@@ -131,7 +218,8 @@ func parsePage(pageRes *PageResult) (stud map[string]string, err error) {
 		return student, err
 	}
 
-	fields := map[string]string{"total": "#ctl00_cphMain_TabContainer1_Marks_txtTotal",
+	fields := map[string]string{
+		"total":      "#ctl00_cphMain_TabContainer1_Marks_txtTotal",
 		"name":       "#ctl00_cphMain_TabContainer1_Marks_txtName",
 		"eng":        "#ctl00_cphMain_TabContainer1_Marks_Gridview1_ctl02_MKS",
 		"kis":        "#ctl00_cphMain_TabContainer1_Marks_Gridview1_ctl03_MKS",
@@ -152,7 +240,7 @@ func parsePage(pageRes *PageResult) (stud map[string]string, err error) {
 		if !ok {
 			return student, errors.New("Bad page")
 		}
-		student[subj] = f
+		student[subj] = strings.TrimSpace(f)
 	}
 	student["index"] = pageRes.Index
 	return student, nil
@@ -160,34 +248,84 @@ func parsePage(pageRes *PageResult) (stud map[string]string, err error) {
 
 func main() {
 
-	tasks := getIndexNumbers()
-	results := make(chan string)
+	client := &http.Client{}
+	counties := getCountyNumbers()
+	fields := []string{"total", "name", "eng",
+		"kis", "mat", "sci", "ssr",
+		"schoolName", "gender", "engGrade", "kisGrade",
+		"matGrade", "sciGrade", "ssrGrade"}
+
+	fmt.Println("total,name,eng,kis,mat,sci,ssr,schoolName,gender,engGrade,kisGrade,matGrade,sciGrade,ssrGrade")
 
 	var wg sync.WaitGroup
-	for i := 0; i < MAXROUTINES; i++ {
+	for countyIndex := range counties {
 		wg.Add(1)
+		fmt.Println("Country index(main): " + countyIndex)
+		//start a routine per county
 		go func() {
-			for index := range tasks {
+			defer wg.Done()
+
+			errorChan := make(chan bool, 1)
+			errorChan <- false
+			defer close(errorChan)
+			//indexes for all candidates in the county
+			indexes := getIndexNumbers(countyIndex, errorChan)
+			for index := range indexes {
+				fmt.Println("index(main): " + index)
 				res, err := getCandidateResults(index, client)
 				if err != nil {
 
 					fmt.Println("Error with the connection")
 
 				}
+				//fmt.Println("here 1")
 				p := &PageResult{Page: res, Index: index}
-				results <- p
 
+				student, err := parsePage(p)
+				//fmt.Println("here 2")
+				fmt.Println(parsePage(p))
+				if err != nil {
+					errorChan <- true
+					fmt.Println("here 3")
+
+				} else {
+					errorChan <- false
+					fmt.Println("here 4")
+					out := ""
+					for i, details := range fields {
+
+						if i < len(fields)-1 {
+							out += student[details] + ","
+						} else {
+
+							out += student[details]
+						}
+					}
+
+					fmt.Println(out)
+
+				}
 			}
-			wg.Done()
+
 		}()
 
 	}
 
 	wg.Wait()
 
-	//get html from results channel and parse/ put them in CSV
+	/*counties := getCountyNumbers()
+	for countyIndex := range counties {
+		fmt.Println("Country index(main): " + countyIndex)
+		errorChan := make(chan bool)
+		indexes := getIndexNumbers(countyIndex, errorChan)
 
-	/*/FOR TESTING
+		for index := range indexes {
+			fmt.Println("index(main): " + index)
+		}
+
+	}*/
+
+	/*FOR TESTING
 	client := &http.Client{}
 	index := "01113118001"
 	res, _ := getCandidateResults(index, client)
